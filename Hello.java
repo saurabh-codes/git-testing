2aae194d2e52a1d96d3488ec046727f04aa32378
Saurabh Agrahari <agraharisaurabh111@gmail.com>
2025-09-20 15:28:13 +0530

    first commit

---
+++ Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! 1");
+        System.out.println("Hello World! 2");
+        System.out.println("Hello World! 3");
+    }
+}
